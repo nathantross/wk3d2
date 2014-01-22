28978e8c10f7c59c3fa04601da63c06d5c3aef81
nathantross <nathantross@gmail.com>
2014-01-21 18:35:17 -0800

    Added database

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,5 +1,8 @@
 class CreaturesController < ApplicationController
     def index
-        render text: "Hello, creatures."
+        # Note it used to say 
+        #   render text: 'Hello, pilots'
+        render :index
     end
+
 end
