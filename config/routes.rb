bd1d95440fc87896528a50f07777187ae15d62e4
nathantross <nathantross@gmail.com>
2014-01-21 18:39:39 -0800

    Added ability to create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 BogApp::Application.routes.draw do
     root to: 'creatures#index'
+
+    # just to be RESTful
+    get '/creatures', to: 'creatures#index'
+
+    # it's a `get` because 
+    #   someone is requesting
+    #   a page with a form
+    get '/creatures/new', to: 'creatures#new'
+
+    post "/creatures", to: "creatures#create"
+
+    # handle the submitted form
+    post '/creatures', to: 'creatures#create'
+
+
 end
